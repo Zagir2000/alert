bb8a5eaad6942d6a22bb991edc5f0a330245cd65
Zagir2000 <zagir2324@gmail.com>
2023-05-03 18:24:26 +0300

    bug fix

---
+++ cmd/server/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func Test_run(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := run(); (err != nil) != tt.wantErr {
+				t.Errorf("run() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
