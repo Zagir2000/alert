439ca71b31f7c79ff48b268b14bac38c866e927c
Zagir2000 <zagir2324@gmail.com>
2023-06-17 21:30:23 +0300

    Check autotest

--- postgresdb/migrations/0001_init.up.sql
+++ postgresdb/migrations/0001_init.up.sql
@@ -2,23 +2,14 @@ BEGIN TRANSACTION;
 
 DO $$
 BEGIN
-    IF EXISTS (
-        SELECT 
-            * 
-        FROM information_schema.tables 
-        WHERE table_name =" __metrics"  AND table_schema = 'public'
-    )
+   
+    CREATE TABLE IF NOT EXISTS metrics (
+        ID TEXT UNIQUE,
+        MTYPE TEXT,
+        DELTA BIGINT,
+        VALUE DOUBLE PRECISION
+    );
 
-    THEN
-        ALTER TABLE "__metrics" RENAME TO metrics;
-    ELSE
-        CREATE TABLE IF NOT EXISTS metrics (
-            ID TEXT UNIQUE,
-            MTYPE TEXT,
-            DELTA BIGINT,
-            VALUE DOUBLE PRECISION
-        );
-    END IF;
     CREATE INDEX IF NOT EXISTS metric_id ON metrics USING hash(ID);
 END $$;
 --
