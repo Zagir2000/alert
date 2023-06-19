377baee62d90c4442ec3cac5c634d249346734da
Zagir2000 <zagir2324@gmail.com>
2023-06-19 17:51:46 +0300

    Bug fix

--- cmd/server/flag.go
+++ cmd/server/flag.go
@@ -25,13 +25,13 @@ func NewFlagVarStruct() *FlagVar {
 func (f *FlagVar) parseFlags() error {
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
-	connStr := "postgres:5432/praktikum-webinar-db?sslmode=disable"
+
 	flag.StringVar(&f.runAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&f.logLevel, "l", "info", "log level")
 	flag.IntVar(&f.storeIntervall, "i", 300, "time interval according to which the current server servers are kept on disk")
 	flag.StringVar(&f.fileStoragePath, "f", "/tmp/metrics-db.json", "full name of the file where the current valuee are saved")
 	flag.BoolVar(&f.restore, "r", true, "value specifying whether or not to load previously saved values from the specified file at server startup")
-	flag.StringVar(&f.databaseDsn, "d", connStr, "connection to databse")
+	flag.StringVar(&f.databaseDsn, "d", "", "connection to databse")
 	flag.StringVar(&f.migrationsDir, "m", "postgresdb/migrations", "migrations to db")
 
 	// парсим переданные серверу аргументы в зарегистрированные переменные
